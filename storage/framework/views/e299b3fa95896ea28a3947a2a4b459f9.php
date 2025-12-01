1974630161aa809ae03f4f22d1a9ecf7c6cb952e
Piya503 <piyaguhathakurta503@gmail.com>
2025-12-01 12:59:01 +0530

    modal fix

--- storage/framework/views/e299b3fa95896ea28a3947a2a4b459f9.php
+++ storage/framework/views/e299b3fa95896ea28a3947a2a4b459f9.php
@@ -41,6 +41,18 @@
                 <?php if(session('error')): ?>
                     <div class="alert alert-danger alert-dismissible fade show"><?php echo e(session('error')); ?></div>
                 <?php endif; ?>
+                
+<?php if($errors->any()): ?>
+    <div class="alert alert-danger alert-dismissible fade show">
+        <strong>There were some problems with your input:</strong>
+        <ul class="mt-2 mb-0">
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li><?php echo e($error); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+<?php endif; ?>
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card">
