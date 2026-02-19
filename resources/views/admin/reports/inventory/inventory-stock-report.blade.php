020586a48bb5f6a32dcfcfb696d0021c35ac534d
shivamconf2024 <shivam@confitechsol.com>
2026-02-19 10:47:18 +0530

    exportPdf or exportExcel

--- resources/views/admin/reports/inventory/inventory-stock-report.blade.php
+++ resources/views/admin/reports/inventory/inventory-stock-report.blade.php
@@ -97,12 +97,17 @@
                                 <div class="col-lg-12">
                                     <div class="card">
                                         <div class="card-body">
-                                            
+                                            <div class="mb-3">
+
+        <button class="btn btn-success" onclick="exportToExcel('inventory-stock-report-table')">Export to Excel</button>
+        <button class="btn btn-danger" onclick="exportToPDF('inventory-stock-report-table')">Export to PDF</button>
+        
+    </div>  
 
 
                                             <!-- Table start -->
                                             <div class="table-responsive table-nowrap">
-                                                <table class="table border">
+                                                <table class="table border" id="inventory-stock-report-table">
                                                     <thead class="thead-light">
                                                         <tr>
 
