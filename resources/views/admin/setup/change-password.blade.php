0ccd4ac7c5e9e4302e21dfee3248b0c157e506a7
Piya503 <piyaguhathakurta503@gmail.com>
2026-03-11 11:20:14 +0530

    update

--- resources/views/admin/setup/change-password.blade.php
+++ resources/views/admin/setup/change-password.blade.php
@@ -3,64 +3,66 @@
 @section('content')
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-
-            <div class="card">
-                <div class="card-header">
-                    <h5>Change Password</h5>
+    <div class="cng_pass_box">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+    
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Change Password</h5>
+                    </div>
+    
+                    <div class="card-body">
+    
+                        @if(session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+    
+                        @if(session('error'))
+                            <div class="alert alert-danger">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+    
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
+    
+                            <div class="mb-3">
+                                <label>Current Password</label>
+                                <input type="password"
+                                       name="current_password"
+                                       class="form-control"
+                                       required>
+                            </div>
+    
+                            <div class="mb-3">
+                                <label>New Password</label>
+                                <input type="password"
+                                       name="new_password"
+                                       class="form-control"
+                                       required>
+                            </div>
+    
+                            <div class="mb-3">
+                                <label>Confirm New Password</label>
+                                <input type="password"
+                                       name="new_password_confirmation"
+                                       class="form-control"
+                                       required>
+                            </div>
+    
+                            <button type="submit" class="btn btn-primary w-100">
+                                Update Password
+                            </button>
+    
+                        </form>
+    
+                    </div>
                 </div>
-
-                <div class="card-body">
-
-                    @if(session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-
-                    @if(session('error'))
-                        <div class="alert alert-danger">
-                            {{ session('error') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <div class="mb-3">
-                            <label>Current Password</label>
-                            <input type="password"
-                                   name="current_password"
-                                   class="form-control"
-                                   required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label>New Password</label>
-                            <input type="password"
-                                   name="new_password"
-                                   class="form-control"
-                                   required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label>Confirm New Password</label>
-                            <input type="password"
-                                   name="new_password_confirmation"
-                                   class="form-control"
-                                   required>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary w-100">
-                            Update Password
-                        </button>
-
-                    </form>
-
-                </div>
+    
             </div>
-
         </div>
     </div>
 </div>
