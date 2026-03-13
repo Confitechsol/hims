4b9d25462a9e397845e580e5e40cb6f97efd5832
saumen-aspire <SAlaha@aspire-infotech.net>
2026-03-13 16:43:30 +0530

    New changes

--- resources/views/admin/ipd/ipd_view.blade.php
+++ resources/views/admin/ipd/ipd_view.blade.php
@@ -3512,6 +3512,7 @@
                                                                     </td>
                                                                     <!-- <td>{{ $taxAmount }}&nbsp;({{ $charge->charge?->taxCategory?->percentage ?? '-' }}%)
                                                                     </td> -->
+                                                                    <td>&nbsp;</td>
                                                                     <td>{{ $amount }}</td>
                                                                     <td>
                                                                         <div class="d-flex gap-2">
