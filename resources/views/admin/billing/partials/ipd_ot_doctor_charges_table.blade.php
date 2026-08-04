7cca041055621b1ff87298b16ebe12d765cbf1ed
saumen-aspire <SAlaha@aspire-infotech.net>
2026-08-04 16:17:39 +0530

    New changes on doctor date range

--- resources/views/admin/billing/partials/ipd_ot_doctor_charges_table.blade.php
+++ resources/views/admin/billing/partials/ipd_ot_doctor_charges_table.blade.php
@@ -3,11 +3,10 @@
     <table class="charges-table">
         <thead>
             <tr>
-                <th colspan="3">Doctor Charge (Anesthesia + Surgeon)</th>
+                <th colspan="2">Doctor Charge (Anesthesia + Surgeon)</th>
             </tr>
             <tr>
                 <th>Doctor</th>
-                <th>Date Range</th>
                 <th class="text-right">Amount (Rs.)</th>
             </tr>
         </thead>
@@ -23,16 +22,11 @@
                             @ {{ number_format($row->rate_per_visit, 2) }}
                         @endif
                     </td>
-                    <td>
-                        {{ $row->from_date ? \Carbon\Carbon::parse($row->from_date)->format('d/m/Y') : 'N/A' }}
-                        To
-                        {{ $row->to_date ? \Carbon\Carbon::parse($row->to_date)->format('d/m/Y') : 'N/A' }}
-                    </td>
                     <td class="text-right">Rs. {{ number_format($row->total_amount ?? 0, 2) }}</td>
                 </tr>
             @endforeach
             <tr style="font-weight: bold;">
-                <td colspan="2" class="text-right">Subtotal:</td>
+                <td class="text-right">Subtotal:</td>
                 <td class="text-right">Rs. {{ number_format($otDoctorChargesDisplaySubtotal ?? $otDoctorChargeRows->sum('total_amount'), 2) }}</td>
             </tr>
         </tbody>
