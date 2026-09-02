ae60b49ff36c084efa822b1660a9f746c966cc87
shivamconf2024 <shivam@confitechsol.com>
2026-09-02 13:12:36 +0530

    pagenation or bug fixed in dicharges

--- resources/views/admin/ipd/index.blade.php
+++ resources/views/admin/ipd/index.blade.php
@@ -248,8 +248,8 @@
                                                         <th>IPD No.</th>
                                                         <th>Name</th>
                                                         <th>Phone</th>
-                                                        <th>Email</th>
-                                                        <th>Address</th>
+                                                        {{-- <th>Email</th> --}}
+                                                        {{-- <th>Address</th> --}}
                                                         <th>Gender</th>
                                                         <th>Consultant</th>
                                                         <th>Discharged Date</th>
@@ -285,8 +285,8 @@
                                                             <td>{{ optional($ipdDetails->patient)->patient_name ?? '-' }}
                                                             </td>
                                                             <td>{{ optional($ipdDetails->patient)->mobileno ?? '-' }}</td>
-                                                            <td>{{ optional($ipdDetails->patient)->email ?? '-' }}</td>
-                                                            <td>{{ optional($ipdDetails->patient)->address ?? '-' }}</td>
+                                                            {{-- <td>{{ optional($ipdDetails->patient)->email ?? '-' }}</td>
+                                                            <td>{{ optional($ipdDetails->patient)->address ?? '-' }}</td> --}}
                                                             <td>{{ optional($ipdDetails->patient)->gender ?? '-' }}</td>
                                                             <td>{{ $ipdDetails->doctor->name ?? '-' }}</td>
                                                             <td>{{ \Carbon\Carbon::parse($ipdDetails->discharged_date)->format('d-M-Y') ?? '-' }}
